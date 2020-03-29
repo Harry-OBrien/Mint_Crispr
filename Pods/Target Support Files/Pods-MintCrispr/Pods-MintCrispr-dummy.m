11989954f2b4a7eb2e459c48eeb9db45fab2ab1f
a64070ho <harry.obrien@student.manchester.ac.uk>
2020-03-29 17:12:38 +0100

    Convert project to flutter

--- Pods/Target Support Files/Pods-MintCrispr/Pods-MintCrispr-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MintCrispr : NSObject
-@end
-@implementation PodsDummy_Pods_MintCrispr
-@end
---
+++ android/app/src/main/kotlin/net/ddns/hobrien/mintcrispr/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.ddns.hobrien.mintcrispr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
