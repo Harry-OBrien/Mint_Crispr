26d5f60d731b9a77ad0d377f53bf309df32ca017
a64070ho <harry.obrien@student.manchester.ac.uk>
2020-04-09 17:32:01 +0100

    Add bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,22 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:mintcrispr/screens/home_screen.dart';
 
-void main() {
-  runApp(MintCrisprApp());
-}
+void main() => runApp(MintCrisprApp());
 
-class MintCrisprApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new KnownDeviceState();
-  }
-}
-
-class KnownDeviceState extends State<MintCrisprApp> {
-  var _isLoading = true;
-  var devices;
-
-  _fetchData() async {
-    print("Fetching data from network!");
-
-    final url = "localhost://devices.php";
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      var map = jsonDecode(response.body);
-      print(map);
-    }
-
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchData();
-  }
-
+class MintCrisprApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: new Scaffold(
-            appBar: new AppBar(
-              title: new Text("Devices"),
-              actions: <Widget>[
-                new IconButton(
-                  icon: new Icon(Icons.add),
-                  onPressed: () {
-                    Navigator.push(context,
-                    new MaterialPageRoute(
-                          builder: (context) => new DetailPage())
-                    );
-                  },
-                )
-              ],
-            ),
-            body: new Center(child: CircularProgressIndicator())));
-  }
-}
-
-
-class DetailPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-          appBar: new AppBar(
-            title: new Text("Detail Page"),
-          ),
-          body: new Center(
-            child: new Text("deety deet deet"),
-          )
+    return MaterialApp(
+      title: 'House Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff53b589),
+        accentColor: Color(0xff53b0b5),
+        buttonColor: Color(0xaa83a9d1),
+        unselectedWidgetColor: Color(0xFFD0EAE7),
+        backgroundColor: Color(0xffefeff4),
+      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: RichText(
+          textAlign: TextAlign.left,
+          text: TextSpan(
+            text: 'Mint',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontFamily: "HelveticaNeue-Condensed",
+              fontWeight: FontWeight.w200,
+              fontSize: 40,
+              letterSpacing: 1.4,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Crispr',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: SafeArea(
+        child: Center(),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+        ]),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10).add(EdgeInsets.only(top: 5)),
+            child: GNav(
+                gap: 10,
+                activeColor: Theme.of(context).primaryColor,
+                color: Colors.grey[400],
+                iconSize: 24,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                duration: Duration(milliseconds: 300),
+                tabBackgroundColor: Colors.grey[800],
+                tabs: [
+                  GButton(
+                    icon: FontAwesomeIcons.tachometerAlt,
+                    text: 'Devices',
+                    backgroundColor:
+                        Theme.of(context).primaryColor.withAlpha(70),
+                  ),
+                  GButton(
+                    icon: FontAwesomeIcons.folderOpen,
+                    text: 'Files',
+                    backgroundColor:
+                        Theme.of(context).primaryColor.withAlpha(70),
+                  ),
+                  GButton(
+                    icon: FontAwesomeIcons.cogs,
+                    text: 'Settings',
+                    backgroundColor:
+                        Theme.of(context).primaryColor.withAlpha(70),
+                  ),
+                ],
+                selectedIndex: 0,
+                onTabChange: (index) {
+                  setState(() {
+                    // pageController.jumpToPage(index);
+                  });
+                }),
+          ),
+        ),
+      ),
+    );
+  }
+}
